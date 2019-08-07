de7b8d6208650953fecf3cfd5ec68506bb77a83f
René Cecil Vidríales Trujillo <vidrialestrujillorene@gmail.com>
2019-08-06 18:25:18 -0600

    Added implicit number casting and rounding before insert, added some modifications to database schema and added way to store query for years and years... more to be added. This is just a beta.

--- db/schema.sql
+++ db/schema.sql
@@ -6,43 +6,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- En ésta tabla se guardarán las predicciones que generará el motor
--- para no estar haciendo la misma consulta en repetidas ocasiones.
-CREATE TABLE Prediction (
-    PredictionID SERIAL PRIMARY KEY,
-    CreatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    UpdatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    PredictedPrice INT NOT NULL, -- El precio que se predice (se guarda en centavos)
-    PredictionDate TIMESTAMPTZ NOT NULL, -- La fecha del precio que se predice
-    SourceMarketID INT NOT NULL, -- El ID del mercado de orígen
-    EndMarketID INT NOT NULL, -- El ID del mercado de destino
-    FOREIGN KEY (SourceMarketID) REFERENCES Market(MarketID),
-    FOREIGN KEY (EndMarketID) REFERENCES Market(MarketID),
-);
-
--- En ésta tabla se almacenarán los precios tal cual aparecen en el SNIIM
--- Nota: los precios se almacenarán en centavos, es decir:
--- si la papa cuesta 13.50xKG en base de datos se almacenará como 13.50 * 100 = 1350
--- esto debido a que existen bases de datos que no mantienen buena integridad con los números
--- con punto decimal, de ésta manera, en centavos, solamente se necesitaría multiplicar x100 al
--- momento de retornar la respuesta.
-CREATE TABLE Price (
-    PriceID SERIAL PRIMARY KEY,
-    CreatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    UpdatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    MinPrice INT NOT NULL, -- Precio mínimo
-    MaxPrice INT NOT NULL, -- Precio máximo
-    AvgPrice INT NOT NULL, -- Precio frecuente
-    Observations VARCHAR NULL, -- Observaciones realizadas por el SNIIM
-    SourceMarketID INT NOT NULL, -- El ID del mercado de orígen
-    EndMarketID INT NOT NULL, -- El ID del mercado de destino
-    TypeOfPrice INT NOT NULL, -- El tipo de precio (presentación comercial o kilogramo calculado)
-    PotatoID INT NOT NULL, -- El ID del tipo de la papa a la cual le corresponde el precio
-    FOREIGN KEY (SourceMarketID) REFERENCES Market(MarketID),
-    FOREIGN KEY (EndMarketID) REFERENCES Market(MarketID),
-    FOREIGN KEY (PotatoID) REFERENCES Potato(PotatoID)
-);
-
 -- Esta tabla almacenará los mercados (orígenes y destinos)
 CREATE TABLE Market (
     MarketID SERIAL PRIMARY KEY,
@@ -58,6 +21,45 @@ CREATE TABLE Potato (
     UpdatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     PotatoSNIIMID INT NOT NULL,
     PotatoName VARCHAR(20) NOT NULL
+);
+
+-- En ésta tabla se almacenarán los precios tal cual aparecen en el SNIIM
+-- Nota: los precios se almacenarán en centavos, es decir:
+-- si la papa cuesta 13.50xKG en base de datos se almacenará como 13.50 * 100 = 1350
+-- esto debido a que existen bases de datos que no mantienen buena integridad con los números
+-- con punto decimal, de ésta manera, en centavos, solamente se necesitaría multiplicar x100 al
+-- momento de retornar la respuesta.
+CREATE TABLE Price (
+    PriceID SERIAL PRIMARY KEY,
+    CreatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    UpdatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    SNIIMDate VARCHAR NOT NULL, -- Fecha que aparece en el SNIIM DD/MM/YYYY
+    MinPrice INT NOT NULL, -- Precio mínimo
+    MaxPrice INT NOT NULL, -- Precio máximo
+    AvgPrice INT NOT NULL, -- Precio frecuente
+    Observations VARCHAR NULL, -- Observaciones realizadas por el SNIIM
+    SourceMarketID INT NOT NULL, -- El ID del mercado de orígen
+    EndMarketID INT NOT NULL, -- El ID del mercado de destino
+    SNIIMPresentation INT NOT NULL, -- El tipo de precio que da el SNIIM (presentación comercial o kilogramo calculado)
+    MarketPresentation VARCHAR NOT NULL, -- El tipo de presentación en el mercado (kilogramos, arpillas, etc)
+    PotatoID INT NOT NULL, -- El ID del tipo de la papa a la cual le corresponde el precio
+    FOREIGN KEY (SourceMarketID) REFERENCES Market(MarketID),
+    FOREIGN KEY (EndMarketID) REFERENCES Market(MarketID),
+    FOREIGN KEY (PotatoID) REFERENCES Potato(PotatoID)
+);
+
+-- En ésta tabla se guardarán las predicciones que generará el motor
+-- para no estar haciendo la misma consulta en repetidas ocasiones.
+CREATE TABLE Prediction (
+    PredictionID SERIAL PRIMARY KEY,
+    CreatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    UpdatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    PredictedPrice INT NOT NULL, -- El precio que se predice (se guarda en centavos)
+    PredictionDate TIMESTAMPTZ NOT NULL, -- La fecha del precio que se predice
+    SourceMarketID INT NOT NULL, -- El ID del mercado de orígen
+    EndMarketID INT NOT NULL, -- El ID del mercado de destino
+    FOREIGN KEY (SourceMarketID) REFERENCES Market(MarketID),
+    FOREIGN KEY (EndMarketID) REFERENCES Market(MarketID)
 );
 
 CREATE TRIGGER set_timestamp
--- scrapper.js
+++ scrapper.js
@@ -6,13 +6,28 @@ const Queries = require('./queries.json');
 const config = require('./sniimConfig.json');
 const { sendQuery, disconnectPool } = require('./db');
 
-function generateURL(typeOfPrice = 1, yearsFromNow = 1, rowsPerPage = 1000) {
-  const yesterday = `${moment().format('D')}/${moment().month() + 1}/${moment().subtract(yearsFromNow, 'years').year()}`;
-  const today = `${moment().format('D')}/${moment().month() + 1}/${moment().year()}`;
-  return `${config.baseURL}&fechaInicio=${yesterday}&fechaFinal=${today}&PreciosPorId=${typeOfPrice}&RegistrosPorPagina=${rowsPerPage}`;
+function generateURL(typeOfPrice = 1, rowsPerPage = 1000, year) {
+  const currentYear = `${moment().format('D')}/${moment().month() + 1}/${year}`;
+  const nextYear = `${moment().format('D')}/${moment().month() + 1}/${year + 1}`;
+  return `${config.baseURL}&fechaInicio=${currentYear}&fechaFinal=${nextYear}&PreciosPorId=${typeOfPrice}&RegistrosPorPagina=${rowsPerPage}`;
 }
 
-async function crawlWebPage(url) {
+const markets = [];
+async function createAndFetchMarket(name) {
+  const foundMarket = markets.find((market) => market.marketname === name);
+  if (foundMarket) return foundMarket;
+  try {
+    const [market] = await sendQuery(Queries.market.INSERT_MARKET, [name]);
+    console.info(`Market ${market.marketname} created`);
+    markets.push(market);
+    return market;
+  } catch (error) {
+    console.log(error);
+    process.exit(-1);
+  }
+}
+
+async function crawlWebPage(url, sniimPriceType) {
   const potatoes = await sendQuery(Queries.potatoes.FETCH_ALL_POTATOES);
   for (const potato of potatoes) {
     const $ = await rp({
@@ -24,14 +39,30 @@ async function crawlWebPage(url) {
     });
     const tableRows = $('#tblResultados > tbody');
     if (tableRows && tableRows['0']) {
+      const rows = [];
       for (const child of tableRows['0'].children) {
-        if (child.type && child.name && child.attribs && child.children) {
-          const { type, name, attribs, children: childChildren } = child;
-          for (const childChild of childChildren) {
-            if (childChild.children) {
-              console.log({ ch: childChild.children });
-            }
-          }
+        if (child.type && child.name && child.attribs && child.children && child.children.length) {
+          const normalChilds = child.children.map(c => {
+            if (c !== undefined && c.children && c.children.length) return c.children[0].data
+          }).filter((element) => (element !== undefined));
+          rows.push(normalChilds);
+        }
+      }
+      for (const row of rows) {
+        const rowLength = row.length;
+        if (rowLength === 7 || rowLength === 8 && row[0] !== 'Fecha') {
+          const sourceMarket = await createAndFetchMarket(row[2]);
+          const endMarket = await createAndFetchMarket(row[3]);
+          const values = [...row];
+          values.splice(2, 2);
+          values[2] = values[2].replace(',', '');
+          values[3] = values[3].replace(',', '');
+          values[4] = values[4].replace(',', '');
+          values[2] = Math.round(Number(values[2]) * 100);
+          values[3] = Math.round(Number(values[3]) * 100);
+          values[4] = Math.round(Number(values[4]) * 100);
+          const [returnedRow] = await sendQuery(rowLength === 7 ? Queries.price.INSERT_PRICE_NO_OBS : Queries.price.INSERT_PRICE_OBS, [...values, potato.potatoid, sourceMarket.marketid, endMarket.marketid, sniimPriceType]);
+          console.info(`Price ${returnedRow.priceid} inserted`);
         }
       }
     }
@@ -40,10 +71,14 @@ async function crawlWebPage(url) {
 
 (async () => {
   const presentacionComercial = 1;
-  const urlComercial = generateURL(presentacionComercial, 1, 10000);
   const precioKilogramoCalculado = 2;
-  const urlCalculado = generateURL(precioKilogramoCalculado, 1, 10000);
-  // await crawlWebPage(urlComercial);
-  await crawlWebPage(urlCalculado);
+  let startingYear = 2009;
+  for (let index = 0; index < 10; index++) {
+    const urlComercial = generateURL(presentacionComercial, 30000, startingYear);
+    const urlCalculado = generateURL(precioKilogramoCalculado, 30000, startingYear);
+    await crawlWebPage(urlComercial, presentacionComercial);
+    await crawlWebPage(urlCalculado, precioKilogramoCalculado);
+    startingYear += 1;
+  }
   await disconnectPool();
 })();
